a302954fe8c3f28283ce00ec34a2d81df75bb86a
theonlywisdom <mailbaah@gmail.com>
2020-12-17 09:37:36 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input: ");
+            string s = Console.ReadLine();
+            Console.Write("Output: ");
+            Console.WriteLine(Reverse(s));
+        }
+
+        public static string Reverse(string s)
+        {
+            if (string.IsNullOrEmpty(s) || s.Length == 1)
+            {
+                return s;
+            }
+
+            char[] stringChars = s.ToCharArray();
+            Array.Reverse(stringChars);
+            return new string(stringChars);
+        }
+    }
+}
